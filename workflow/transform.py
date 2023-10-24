bfa3fd3b0cd57321356e0f5111894be9e7cd3ded
Dzmitry Porakhau <dima.porokhov.2000@mail.ru>
2023-10-24 09:47:14 +0300

    change rename mappings

--- workflow/transform.py
+++ workflow/transform.py
@@ -34,57 +34,62 @@ def transform_web():
     print('STARTING CLEANING AND TRANSFORMATIONS:')
     # columns renaming
     web_rename_mapping = {
-        "root_domain": "domain",
-        "legal_name": "company_name",
-        "main_city": "city",
-        "main_country": "country",
-        "main_region": "region",
-        "s_category": "category"
+        "root_domain": "web_domain",
+        "domain_suffix": "web_domain_suffix",
+        "language": "web_language",
+        "legal_name": "web_company_name",
+        "main_city": "web_city",
+        "main_country": "web_country",
+        "main_region": "web_region",
+        "phone": "web_phone",
+        "site_name": "web_site_name",
+        "tld": "web_tld",
+        "s_category": "web_category"
     }
     web_df.rename(web_rename_mapping, axis=1, inplace=True)
 
     # to lowercase
     web_lowercase_cols_list = [
-        "domain", "domain_suffix", "language", "company_name",
-        "city", "country", "region", "site_name", "tld", "category"
+        "web_domain", "web_domain_suffix", "web_language", "web_company_name",
+        "web_city", "web_country", "web_region", "web_site_name", "web_tld", "web_category"
     ]
     for col in web_lowercase_cols_list:
         web_df[col] = web_df[col].str.lower()
 
     # escape special characters
     web_escape_cols_list = [
-        "language", "company_name", "city", "country",
-        "region", "site_name", "tld", "category"
+        "web_language", "web_company_name", "web_city", "web_country",
+        "web_region", "web_site_name", "web_tld", "web_category"
     ]
     for col in web_escape_cols_list:
         web_df[col] = web_df[col].apply(
             lambda x: np.nan if x is np.nan else escape_special_characters(str(x))
         )
-        if col == "company_name":
+        if col == "web_company_name":
             for suffix in COMPANY_SUFFIXES_LIST:
                 web_df[col] = web_df[col].str.replace(suffix, "")
 
     # filter df
     web_df = web_df[
-        web_df['domain'].apply(
+        web_df['web_domain'].apply(
             lambda x: x is np.nan or isinstance(x, str) and bool(re.match(DOMAIN_CHECK_RE, x))
         )
     ]
     web_df = web_df[
-        web_df['domain_suffix'].apply(
+        web_df['web_domain_suffix'].apply(
             lambda x: x is np.nan or isinstance(x, str) and bool(re.match(DOMAIN_SUFFIX_CHECK_RE, x))
         )
     ]
     web_df = web_df[
-        web_df['phone'].apply(
+        web_df['web_phone'].apply(
             lambda x: x is np.nan or isinstance(x, str) and bool(re.match(PHONE_CHECK_RE, x))
         )
     ]
 
-    web_df.dropna(subset=['domain'], inplace=True)
+    web_df.dropna(subset=['web_domain'], inplace=True)
 
     # cast columns
-    web_df['phone'] = web_df['phone'].astype(float, errors='ignore').astype(int, errors='ignore')
+    web_df['web_phone'] = web_df['web_phone'].astype(float, errors='ignore').astype(int, errors='ignore')
 
     # drop duplicates
     web_df.drop_duplicates(inplace=True)
@@ -105,17 +110,30 @@ def transform_fb():
     print('STARTING CLEANING AND TRANSFORMATIONS:')
     # columns renaming
     fb_rename_mapping = {
-        "country_name": "country",
-        "name": "company_name",
-        "region_name": "region"
+        "domain": "fb_domain",
+        "address": "fb_address",
+        "categories": "fb_categories",
+        "city": "fb_city",
+        "country_code": "fb_country_code",
+        "country_name": "fb_country",
+        "description": "fb_description",
+        "email": "fb_email",
+        "link": "fb_link",
+        "name": "fb_company_name",
+        "page_type": "fb_page_type",
+        "phone": "fb_phone",
+        "phone_country_code": "fb_phone_country_code",
+        "region_code": "fb_region_code",
+        "region_name": "fb_region",
+        "zip_code": "fb_zip_code"
     }
     fb_df.rename(fb_rename_mapping, axis=1, inplace=True)
 
     # to lowercase
     fb_lowercase_cols_list = [
-        "domain", "categories", "city", "country_code",
-        "country", "email", "link", "company_name", "page_type",
-        "phone_country_code", "region_code", "region"
+        "fb_domain", "fb_categories", "fb_city", "fb_country_code",
+        "fb_country", "fb_email", "fb_link", "fb_company_name", "fb_page_type",
+        "fb_phone_country_code", "fb_region_code", "fb_region"
     ]
     for col in fb_lowercase_cols_list:
         fb_df[col] = fb_df[col].str.lower()
@@ -123,34 +141,34 @@ def transform_fb():
     # exploding categories into multiple rows
     fb_df = fb_df\
         .assign(
-            category=fb_df['categories'].str.split('|')
+            fb_category=fb_df['fb_categories'].str.split('|')
         )\
-        .explode('category')\
+        .explode('fb_category')\
         .reset_index(drop=True)
 
-    fb_df.drop('categories', axis=1, inplace=True)
+    fb_df.drop('fb_categories', axis=1, inplace=True)
 
     # escape special characters
     fb_escape_cols_list = [
-        "category", "city", "country_code",
-        "country", "company_name", "page_type",
-        "phone_country_code", "region_code", "region"
+        "fb_category", "fb_city", "fb_country_code",
+        "fb_country", "fb_company_name", "fb_page_type",
+        "fb_phone_country_code", "fb_region_code", "fb_region"
     ]
     for col in fb_escape_cols_list:
         fb_df[col] = fb_df[col].apply(
             lambda x: np.nan if x is np.nan else escape_special_characters(str(x))
         )
-        if col == "company_name":
+        if col == "fb_company_name":
             for suffix in COMPANY_SUFFIXES_LIST:
                 fb_df[col] = fb_df[col].str.replace(suffix, "")
 
     # filter df
     fb_df = fb_df[
-        fb_df['domain'].apply(
+        fb_df['fb_domain'].apply(
             lambda x: x is np.nan or isinstance(x, str) and bool(re.match(DOMAIN_CHECK_RE, x))
         )
     ]
-    fb_df.dropna(subset=['domain'], inplace=True)
+    fb_df.dropna(subset=['fb_domain'], inplace=True)
 
     # drop duplicates
     fb_df.drop_duplicates(inplace=True)
@@ -171,40 +189,52 @@ def transform_google():
 
     # columns renaming
     google_rename_mapping = {
-        "country_name": "country",
-        "name": "company_name",
-        "region_name": "region"
+        "address": "gg_address",
+        "category": "gg_category",
+        "city": "gg_city",
+        "country_code": "gg_country_code",
+        "country_name": "gg_country",
+        "name": "gg_company_name",
+        "phone": "gg_phone",
+        "phone_country_code": "gg_phone_country_code",
+        "raw_address": "gg_raw_address",
+        "raw_phone": "gg_raw_phone",
+        "region_code": "gg_region_code",
+        "region_name": "gg_region",
+        "text": "gg_text",
+        "zip_code": "gg_zip_code",
+        "domain": "gg_domain"
     }
     google_df.rename(google_rename_mapping, axis=1, inplace=True)
 
     # to lowercase
     google_lowercase_cols_list = [
-        "domain", "category", "city", "country_code", "country",
-        "company_name", "phone_country_code", "region_code", "region"
+        "gg_domain", "gg_category", "gg_city", "gg_country_code", "gg_country",
+        "gg_company_name", "gg_phone_country_code", "gg_region_code", "gg_region"
     ]
     for col in google_lowercase_cols_list:
         google_df[col] = google_df[col].str.lower()
 
     # escape special characters
     google_escape_cols_list = [
-        "category", "city", "country_code", "country",
-        "company_name", "phone_country_code", "region_code", "region"
+        "gg_category", "gg_city", "gg_country_code", "gg_country",
+        "gg_company_name", "gg_phone_country_code", "gg_region_code", "gg_region"
     ]
     for col in google_escape_cols_list:
         google_df[col] = google_df[col].apply(
             lambda x: np.nan if x is np.nan else escape_special_characters(str(x))
         )
-        if col == "company_name":
+        if col == "gg_company_name":
             for suffix in COMPANY_SUFFIXES_LIST:
                 google_df[col] = google_df[col].str.replace(suffix, "")
 
     # filter df
     google_df = google_df[
-        google_df['domain'].apply(
+        google_df['gg_domain'].apply(
             lambda x: x is np.nan or isinstance(x, str) and bool(re.match(DOMAIN_CHECK_RE, x))
         )
     ]
-    google_df.dropna(subset=['domain'], inplace=True)
+    google_df.dropna(subset=['gg_domain'], inplace=True)
 
     # drop duplicates
     google_df.drop_duplicates(inplace=True)
